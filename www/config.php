ecf2be85e7fda08af79c6601e585f59dab75c116
Evgeniy Pozdnyakov <evpozdniakov@gmail.com>
2011-10-11 11:20:03 +0400

    изменил config.php, добавил кучу комментариев

--- www/config.php
+++ www/config.php
@@ -1,77 +1,95 @@
 <?php
 
-// запрещаем прямой доступ к файлу
+// запрещаем прямой доступ к файлу, чтобы его нельзя было открыть через браузер
+// для безопасности, следует его размещать за пределами DOCUMENT_ROOT
 if(mb_strpos($_SERVER['REQUEST_URI'],'/config.php')!==false){
 	header($_SERVER["SERVER_PROTOCOL"].' 403 Forbidden');
 	exit();
 }
 
-//DB
+/*MySQL DB*/
 define('DBHOST','localhost');
 define('DBNAME','');
 define('DBUSER','');
 define('DBPASSWORD','');
 define('DBSETNAMES','utf8');
 
-//субдомены
+/*субдомены*/
+// будут ли использоваться субдомены 
+// если false, то все остальные опции — SUBDOMAINS_LIST DEFAULT_SUBDOMAIN HIDE_DEFAULT_SUBDOMAIN — не имеют значения
 define('USE_SUBDOMAINS',true);
-define('SUBDOMAINS_LIST','msk spb');
-define('DEFAULT_SUBDOMAIN','msk');
+// список используемых субдоменов (через пробел)
+// например, если будут использоваться www.site.ru и sale.site.ru
+// то нужно указать 'www sale'
+define('SUBDOMAINS_LIST','www sale');
+// основной субдомен (субдомен по-умолчанию)
+// на него будет происходить редирект если адрес сайта набрали без субдомена
+// site.ru -> www.site.ru
+define('DEFAULT_SUBDOMAIN','www');
+// следует ли скрывать основной субдомен
+// чтобы избежать редиректа на основной субдомен при запросе сайта без субдомена
+// но в этом случае будет происходить редирект 
+// www.site.ru -> site.ru
 define('HIDE_DEFAULT_SUBDOMAIN',true);
 
-// ключ yandex-карт
+/*ключ yandex-карт*/
+// используем переменную окружения SERVER_NAME чтобы задать разные ключи к картам
+// в зависимости от адреса сайта
+// (имейте в виду, что в SERVER_NAME не всегда может находиться имя сайта — это зависит от настроек веб-сервера)
 if( $_SERVER['SERVER_NAME']=='site.local' ){
-	define('MAPS_YANDEX_API_KEY','123098y41902hrp91hp9hr99r1-29834912y3-48234');
+	define('MAPS_YANDEX_API_KEY','AAAAAAAAABBBBBBBBCCCCCCCCCDDDDDDDDEEEEEEEFFFFFFFFFF');
 }elseif( $_SERVER['SERVER_NAME']=='site.ru' ){
-	define('MAPS_YANDEX_API_KEY','090-190r7qwe0987rq0w9e87r90qw9876er98qw6e98r769871623948672');
+	define('MAPS_YANDEX_API_KEY','AAAAAAAAABBBBBBBBCCCCCCCCCDDDDDDDDEEEEEEEFFFFFFFFFF');
 }
 
-//почта
-define('SITE_ENCODING','UTF-8');//кодировка сайта (windows-1251|UTF-8) нужна для корректной отправки писем
+/*почта*/
+// кодировка сайта (windows-1251|UTF-8) нужна для корректной отправки писем
+define('SITE_ENCODING','UTF-8');
 define('FROM_NAME','Вася Пупкин');
 define('FROM_EMAIL','no-reply@site.ru');
+// следует ли записывать параметры отправляемых писем в лог /admin/_fw.log
 define('LOG_EMAIL',true);
 
-// sms
-define('SMSPILOT_APIKEY','12-034-129U3-0R10UR0QW9UE0RQ04JHR0QWHE0RQ0W');
+/*sms*/
+define('SMSPILOT_APIKEY','AAAAAAAAABBBBBBBBCCCCCCCCCDDDDDDDDEEEEEEEFFFFFFFFFF');
 define('SMS_SENDER','V.Pupkin');
+// следует ли записывать параметры отправляемых sms в лог /admin/_fw.log
 define('LOG_SMS',true);
-
 
 //шифрование
 define('CRYPT_METHOD','sha1');
 
-//кэширование
-// define('USE_CACHE',false);
-// define('AJAX_CACHE_ON',true);
-// define('CACHE_DIR',$_SERVER['DOCUMENT_ROOT'].'/__cache/');
+/*кэширование*/
+define('USE_CACHE',false);
 
-//smarty
+/*smarty*/
+// путь к библиотеке
 define('SMARTY_DIR',$_SERVER['DOCUMENT_ROOT'].'/../__smarty/Smarty-3.0.8/libs/');
-// define('SMARTY_DIR',$_SERVER['DOCUMENT_ROOT'].'/../__smarty/Smarty-2.6.26/libs/');
+// путь к директории, в которой лежит папка с шаблонами
 define('TPL_DIR',$_SERVER['DOCUMENT_ROOT'].'/../__smarty/');
+// синтаксис тегов шаблонов (left_delimiter и right_delimiter разделены пробелом)
 define('SMARTY_SYNTAX', '{ }');
 
-//библиотеки
+/*библиотеки*/
 define('LIB_DIR',$_SERVER['DOCUMENT_ROOT'].'/../__lib/');
 
-
-//отладка
-// define('ADMIN_EMAIL','admin@site.ru');
+/*отладка*/
+// e-mail администратора (можно указать несколько адресов через пробел)
+// сюда будут дублироваться сообщения из логов /admin/*.log
+define('ADMIN_EMAIL','admin@site.ru');
+// режим отладки
+// если выключен, то 
+// 1. никакие сообщения об ошибках не выводятся на экран
+// 2. не работает функция _log()
 define('DEBUG',true);
-// define('DEBUG_TRACE_KEY','1');
-// define('DEBUG_OPTIONS', 'DISPLAY_PHP_ERRORS,TRACE_ON_DIE,DB,DB_SAVE_STACK,DB_INCLUDE_RES');
-define('DEBUG_OPTIONS', 'DISPLAY_PHP_ERRORS,TRACE_ON_DIE');
-/**
- * !defined('DEBUG') or !DEBUG запрещают любую отладку
- * Доступные опции отладки (DEBUG_OPTIONS - ключи отделённые запятыми):
- * DB - включить отладку запросов к DB
- * DB_SAVE_STACK - при отладке DB сохранять стек вызовов функций
- * DB_INCLUDE_RES - при отладке DB накапливать и выводить результаты запросов
- * SMARTY_CONSOLE_ON - включить консоль Smarty
- * TRACE_ON_DIE - вывести отладку после отработки die()
- * DISPLAY_PHP_ERRORS - вывести ошибки php на экран (в любом случае выводятся в /admin/_php.log)
- * START_ON_KEY - начать отладку только после получения $_GET['TRACE_KEY']==DEBUG_TRACE_KEY 
- *                отключить отладку можно передав $_GET['TRACE_KEY']==false (другие значения могут иметь специфическое назначение)
- *                если опция отсутствует, то управление отладкой (вкл/выкл) осуществляется только через DEBUG
- */
+// если режим отладки включен, то можно настроить опции:
+// DB - включить отладку запросов к DB
+// DB_SAVE_STACK - при отладке DB сохранять стек вызовов функций
+// DB_INCLUDE_RES - при отладке DB накапливать и выводить результаты запросов
+// SMARTY_CONSOLE_ON - включить консоль Smarty
+// TRACE_ON_DIE - вывести отладку после отработки die()
+// DISPLAY_PHP_ERRORS - вывести ошибки php на экран (в любом случае выводятся в /admin/_php.log)
+// START_ON_KEY - начать отладку только после получения $_GET['TRACE_KEY']==DEBUG_TRACE_KEY 
+//                отключить отладку можно передав $_GET['TRACE_KEY']==false (другие значения могут иметь специфическое назначение)
+//                если опция отсутствует, то управление отладкой (вкл/выкл) осуществляется только через DEBUG
+define('DEBUG_OPTIONS', 'DISPLAY_PHP_ERRORS TRACE_ON_DIE');
